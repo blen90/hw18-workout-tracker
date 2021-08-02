980e4440c4582b5ce5bcf17297449033e32d4613
Blenda Orellana <blen.or90@gmail.com>
2021-08-02 18:46:21 -0500

    created models

---
+++ models/workout.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            name: {
+                type: String,
+                trim: true,
+                required: "Name is required"
+
+            },
+            type: {
+                type: String,
+                trim: true,
+                required: "Type is required"
+
+            },
+            duration: {
+                type: String,
+
+            },
+
+            weight: {
+                type: Number,
+                required: "Weight is required", 
+
+            sets: {
+                type: Number,
+                required: "Sets are required",
+
+            },
+            reps: {
+                type: Number,
+                required: "Reps are required",
+
+            }
+        }
+
+    }
+]
+
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
