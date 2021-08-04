e0a82377e847411b2b96d899f892f1464d244008
Blenda Orellana <blen.or90@gmail.com>
2021-08-04 17:38:25 -0500

    fixed api route and inex.js

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout");
+const Workout = require('../models/Workout');
 
 //Render last workout and adding total duration
 router.get("/api/workouts", (req, res) => {
