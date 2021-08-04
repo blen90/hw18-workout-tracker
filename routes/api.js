7fa70691774b3782e56ae568638730bca3a58582
Blenda Orellana <blen.or90@gmail.com>
2021-08-04 09:51:02 -0500

    fixed the render last workout api route

--- routes/api.js
+++ routes/api.js
@@ -5,14 +5,16 @@ const Workout = require("../models/Workout.js");
 
 //Render last workout
 router.get("/api/workouts", (req, res) => {
-    Workout.find({})
+    console.log("route hit")
+    Workout.aggregate([{$addFields: {totalDuration: {$sum: '$exercises.duration' }}}])
+  
         .then(dbWorkout => {
+            console.log("workout", dbWorkout);
             res.json(dbWorkout);
         })
         .catch(err => {
-            res.json(err);
-
-
+            console.log('error', err)
+            res.status(400).json(err);
         });
 });
 
@@ -49,9 +51,11 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
 //Get Range
 
 router.get('/api/workouts/range', (req, res) => {
-    Workout.aggregate([{$addFields: {'totalDuration': {$sum: '$exercises.duration' }}}])
+    console.log("route hit")
+    Workout.aggregate([{$addFields: {totalDuration: {$sum: '$exercises.duration' }}}])
         .sort({ _id: -1}).limit(7)
         .then(dbWorkout => {
+            console.log("workout", dbWorkout);
             res.json(dbWorkout);
         })
         .catch(err => {
